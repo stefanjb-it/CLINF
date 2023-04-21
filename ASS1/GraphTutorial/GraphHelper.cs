d2307300dbdc5f4c1ab238e1c84256f5f1e2763c
stefanjb-it <stefanjb76@gmail.com>
2023-04-21 15:35:18 +0200

    Added ASS1

---
+++ ASS1/GraphTutorial/GraphHelper.cs
@@ -0,0 +1,154 @@
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Me.SendMail;
+
+class GraphHelper {
+    // Settings object
+    private static Settings? _settings;
+    // User auth token credential
+    private static DeviceCodeCredential? _deviceCodeCredential;
+    // Client configured with user authentication
+    private static GraphServiceClient? _userClient;
+
+    public static void InitializeGraphForUserAuth(Settings settings,
+    Func<DeviceCodeInfo, CancellationToken, Task> deviceCodePrompt) {
+        _settings = settings;
+
+        var options = new DeviceCodeCredentialOptions {
+            ClientId = settings.ClientId,
+            TenantId = settings.TenantId,
+            DeviceCodeCallback = deviceCodePrompt,
+        };
+
+        _deviceCodeCredential = new DeviceCodeCredential(options);
+
+        _userClient = new GraphServiceClient(_deviceCodeCredential, settings.GraphUserScopes);
+    }
+
+    public static async Task<string> GetUserTokenAsync() {
+        // Ensure credential isn't null
+        _ = _deviceCodeCredential ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        // Ensure scopes isn't null
+        _ = _settings?.GraphUserScopes ?? throw new System.ArgumentNullException("Argument 'scopes' cannot be null");
+
+        // Request token with given scopes
+        var context = new TokenRequestContext(_settings.GraphUserScopes);
+        var response = await _deviceCodeCredential.GetTokenAsync(context);
+        return response.Token;
+    }
+
+    public static Task<User?> GetUserAsync() {
+        // Ensure client isn't null
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        return _userClient.Me.GetAsync((config) =>
+        {
+            // Only request specific properties
+            config.QueryParameters.Select = new[] {"displayName", "mail", "userPrincipalName" };
+        });
+    }
+    public static Task<MessageCollectionResponse?> GetInboxAsync() {
+        // Ensure client isn't null
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        return _userClient.Me
+            // Only messages from Inbox folder
+            .MailFolders["Inbox"]
+            .Messages
+            .GetAsync((config) =>
+            {
+                // Only request specific properties
+                config.QueryParameters.Select = new[] { "from", "isRead", "receivedDateTime", "subject" };
+                // Get at most 25 results
+                config.QueryParameters.Top = 25;
+                // Sort by received time, newest first
+                config.QueryParameters.Orderby = new[] { "receivedDateTime DESC" };
+            });
+    }
+    public static async Task SendMailAsync(string subject, string body, string recipient)
+    {
+    // Ensure client isn't null
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        // Create a new message
+        var message = new Message
+        {
+            Subject = subject,
+            Body = new ItemBody
+            {
+                Content = body,
+                ContentType = BodyType.Text
+            },
+            ToRecipients = new List<Recipient>
+            {
+                new Recipient
+                {
+                    EmailAddress = new EmailAddress
+                    {
+                        Address = recipient
+                    }
+                }
+            }
+        };
+
+        // Send the message
+        await _userClient.Me
+            .SendMail
+            .PostAsync(new SendMailPostRequestBody
+            {
+                Message = message
+            });
+    }
+    public static Task<EventCollectionResponse?> GetCalenderEvents()
+    {
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+        
+        return _userClient.Me.Calendar.Events.GetAsync();
+
+    }
+    public async static Task CreateCalenderEvent(String subject, String content, String StartTime, String EndTime, String Location)
+    {
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+        
+        var requestBody = new Event {
+
+            Subject = subject,
+            Body = new ItemBody {
+		        ContentType = BodyType.Html,
+		        Content = content,
+            },
+            Start = new DateTimeTimeZone {
+                DateTime = StartTime,
+                TimeZone = "Europe/Berlin"
+            },
+            End = new DateTimeTimeZone {
+                DateTime = EndTime,
+                TimeZone = "Europe/Berlin"
+            },
+            Location = new Location {
+                DisplayName = Location,
+            },
+            Attendees = new List<Attendee> {
+		        new Attendee {
+			        EmailAddress = new EmailAddress {
+				        Address = "stefan.joebstl@edu.fh-joanneum.at",
+				        Name = "Stefan Joebstl",
+			        },
+			        Type = AttendeeType.Required,
+		        },
+	        }
+        };
+        var result = await _userClient.Me.Calendar.Events.PostAsync(requestBody);
+
+        Console.Write(result);
+    }
+}
---
+++ ASS1/GraphTutorial/Program.cs
@@ -0,0 +1,213 @@
+﻿Console.WriteLine(".NET Graph Tutorial\n");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+InitializeGraph(settings);
+
+// Greet the user by name
+await GreetUserAsync();
+
+int choice = -1;
+
+while (choice != 0)
+{
+    Console.WriteLine("Please choose one of the following options:");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Display access token");
+    Console.WriteLine("2. List my inbox");
+    Console.WriteLine("3. Send mail");
+    Console.WriteLine("4. List calender events");
+    Console.WriteLine("5. Create a calender event");
+
+    try
+    {
+        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+    }
+    catch (System.FormatException)
+    {
+        // Set to invalid value
+        choice = -1;
+    }
+
+    switch(choice)
+    {
+        case 0:
+            // Exit the program
+            Console.WriteLine("Goodbye...");
+            break;
+        case 1:
+            // Display access token
+            await DisplayAccessTokenAsync();
+            break;
+        case 2:
+            // List emails from user's inbox
+            await ListInboxAsync();
+            break;
+        case 3:
+            // Send an email message
+            await SendMailAsync();
+            break;
+        case 4:
+            // List calender events
+            await ListCalenderEvents();
+            break;
+        case 5:
+            // Create a calender event
+            await CreateCalenderEvent();
+            break;
+        default:
+            Console.WriteLine("Invalid choice! Please try again.");
+            break;
+    }
+}
+void InitializeGraph(Settings settings) {
+    GraphHelper.InitializeGraphForUserAuth(settings,
+        (info, cancel) => {
+            // Display the device code message to
+            // the user. This tells them
+            // where to go to sign in and provides the
+            // code to use.
+            Console.WriteLine(info.Message);
+            return Task.FromResult(0);
+        }
+    );
+}
+
+async Task GreetUserAsync()
+{
+    try
+    {
+        var user = await GraphHelper.GetUserAsync();
+        Console.WriteLine($"Hello, {user?.DisplayName}!");
+        // For Work/school accounts, email is in Mail property
+        // Personal accounts, email is in UserPrincipalName
+        Console.WriteLine($"Email: {user?.Mail ?? user?.UserPrincipalName ?? ""}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user: {ex.Message}");
+    }
+}
+
+async Task DisplayAccessTokenAsync()
+{
+    try
+    {
+        var userToken = await GraphHelper.GetUserTokenAsync();
+        Console.WriteLine($"User token: {userToken}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user access token: {ex.Message}");
+    }
+}
+
+async Task ListInboxAsync()
+{
+     try
+    {
+        var messagePage = await GraphHelper.GetInboxAsync();
+
+        if (messagePage?.Value == null)
+        {
+            Console.WriteLine("No results returned.");
+            return;
+        }
+
+        // Output each message's details
+        foreach (var message in messagePage.Value)
+        {
+            Console.WriteLine($"Message: {message.Subject ?? "NO SUBJECT"}");
+            Console.WriteLine($"  From: {message.From?.EmailAddress?.Name}");
+            Console.WriteLine($"  Status: {(message.IsRead!.Value ? "Read" : "Unread")}");
+            Console.WriteLine($"  Received: {message.ReceivedDateTime?.ToLocalTime().ToString()}");
+        }
+
+        // If NextPageRequest is not null, there are more messages
+        // available on the server
+        // Access the next page like:
+        // var nextPageRequest = new MessagesRequestBuilder(messagePage.OdataNextLink, _userClient.RequestAdapter);
+        // var nextPage = await nextPageRequest.GetAsync();
+        var moreAvailable = !string.IsNullOrEmpty(messagePage.OdataNextLink);
+
+        Console.WriteLine($"\nMore messages available? {moreAvailable}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user's inbox: {ex.Message}");
+    }
+}
+
+async Task SendMailAsync()
+{
+    try
+    {
+        // Send mail to the signed-in user
+        // Get the user for their email address
+        var user = await GraphHelper.GetUserAsync();
+
+        var userEmail = user?.Mail ?? user?.UserPrincipalName;
+
+        if (string.IsNullOrEmpty(userEmail))
+        {
+            Console.WriteLine("Couldn't get your email address, canceling...");
+            return;
+        }
+
+        await GraphHelper.SendMailAsync("Testing Microsoft Graph",
+            "Hello world!", userEmail);
+
+        Console.WriteLine("Mail sent.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error sending mail: {ex.Message}");
+    }
+}
+
+async Task ListCalenderEvents()
+{
+    try {
+
+        var events = await GraphHelper.GetCalenderEvents();
+
+        if (events?.Value == null)
+        {
+            Console.WriteLine("No events returned.");
+            return;
+        }
+
+        foreach (var ivent in events.Value)
+        {
+            Console.WriteLine($"Subject: {ivent.Subject}");
+            Console.WriteLine($"Start: {ivent.Start.DateTime}");
+            Console.WriteLine($"End: {ivent.End.DateTime}");
+            Console.WriteLine($"Body: {ivent.BodyPreview}");
+            Console.WriteLine($"Online: {ivent.IsOnlineMeeting}");
+            Console.WriteLine($"Calender: {ivent.Calendar}");
+        }
+
+    } 
+    catch (Exception ex) {
+        Console.WriteLine($"Error recieving calender events: {ex.Message}");
+    }
+}
+async Task CreateCalenderEvent()
+{
+    try {
+
+        await GraphHelper.CreateCalenderEvent(
+            "Testing",
+            "Test of ASS1 Client",
+            "2023-04-25T14:00:00",
+            "2023-04-25T15:00:00",
+            "FH Joanneum"
+        );
+
+        Console.WriteLine("Created event");
+    } catch (Exception ex) {
+        Console.WriteLine($"Error creating event: {ex.Message}");
+    }
+
+}
